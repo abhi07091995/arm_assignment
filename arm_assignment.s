1c4b509338ae848a501442d06e1b8711bcb0bd81
abhi07091995 <53115647+abhi07091995@users.noreply.github.com>
2020-10-01 16:34:12 +0530

    Add files via upload

---
+++ arm_assignment.s
@@ -0,0 +1,17 @@
+     AREA     appcode, CODE, READONLY
+     EXPORT __main
+	 ENTRY 
+
+__main  FUNCTION		         
+         mov r2,#3
+	 mov r7,#4
+	 cmp r2,r7
+	 ITTE NE
+	 subne r2,r2,r7
+	 movne r2,#0
+	 addeq r2,r2,r4
+	 moveq r4,#0
+stop	B stop; stop program	 
+		 
+     ENDFUNC
+     END
